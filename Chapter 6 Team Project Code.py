42899f3d80d7ce787de9a41110a21f09dd888f89
Oliver Albert <oliver.albert915@gmail.com>
2025-11-20 11:53:02 -0600

    Add files via upload

---
+++ Chapter 6 Team Project Code.py
@@ -0,0 +1,20 @@
+def contact_manager():
+    contact_menu()
+    
+def contact_menu():
+    pass
+    
+def contact_add():
+    pass
+    
+def contact_search():
+    pass
+    
+def contact_edit():
+    pass 
+    
+def contact_delete():
+    pass 
+    
+def contact_display():
+    pass
