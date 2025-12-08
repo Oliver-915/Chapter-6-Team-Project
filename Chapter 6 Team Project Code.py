73e6aeb41e28f0ef9fc16a86cfa0c246d09070fb
FloppyFishW <128446643+FloppyFishW@users.noreply.github.com>
2025-12-08 11:42:57 -0600

    Update Chapter 6 Team Project Code.py

--- Chapter 6 Team Project Code.py
+++ Chapter 6 Team Project Code.py
@@ -8,7 +8,21 @@ def contact_add():
     pass
     
 def contact_search():
-    pass
+    # main accepts no arguments
+    # asks for a string
+    # looks for the string in contacts.txt + verification
+    # loop 2-3 if not found
+    # if found, return entire contact and ask the user to confirm.
+    
+    # prime loop
+    choice = "0"
+    contact_file = open('contacts.txt', 'r')
+    
+    #loop while choice is unconfirmed
+    while choice.lower() != "q":
+        search = str(input("Enter a Name, Address, Phone Number, or Email Address (q to quit): "))
+        
+        
     
 def contact_edit():
     pass 
